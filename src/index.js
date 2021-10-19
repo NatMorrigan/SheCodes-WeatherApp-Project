87150d9a678859770db8b1d0cb4ce3cbb6715717
NatMorrigan <91116178+NatMorrigan@users.noreply.github.com>
2021-10-19 08:36:14 +0200

    changes names of the day to full

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function formatDate(date) {
   function formatDay(timestamp) {
     let date = new Date(timestamp * 1000);
     let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let days = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
     return days[day];
   }
   function displayForecast(response){
