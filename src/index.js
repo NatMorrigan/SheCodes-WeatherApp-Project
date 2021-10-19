a84734758d917b0406cbc1722f65fdb8b7acc256
NatMorrigan <91116178+NatMorrigan@users.noreply.github.com>
2021-10-19 08:52:27 +0200

    deleted console.log

--- src/index.js
+++ src/index.js
@@ -61,7 +61,6 @@ function formatDate(date) {
     forecastElement.innerHTML = forecastHTML;
   }
   function getForecast(coordinates) {
-    console.log(coordinates);
     let apiKey = "ab8e7ef210556986d1c9a75d6007b825";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
